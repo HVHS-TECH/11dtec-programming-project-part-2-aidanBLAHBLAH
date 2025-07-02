303e5c68b1f811df16592bdddef9a8a5d6f930d5
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-07-02 23:49:54 +0000

    Jul 2, 2025, 11:49 PM

--- index.js
+++ index.js
@@ -9,30 +9,30 @@ let insurance = 0;
     function getSeats() {
   if (document.getElementById("1seat").checked) {
     seats = "1 seat";
-    document.getElementById("out").innerHTML = "Our options for our one seaters are:\nBac Mono at $25/day" 
+    document.getElementById("seats").innerHTML = "Our options for our one seaters are:\nBac Mono at $25/day" 
   } else if (document.getElementById("2seats").checked){
     seats = "2 seats";
-    document.getElementById("out").innerHTML = "Our options for our two seaters are:\nMXC at $45/day"
+    document.getElementById("seats").innerHTML = "Our options for our two seaters are:\nMXC at $45/day"
     document.body.append(MX5)
   } else if (document.getElementById("4seats").checked){
     seats = "4 seats";
-    document.getElementById("out").innerHTML = "Our options for our four seaters are:\nMini at $30/day\ni30 at $35/day"
+    document.getElementById("seats").innerHTML = "Our options for our four seaters are:\nMini at $30/day\ni30 at $35/day"
   } else if (document.getElementById("5seats").checked){
     seats = "5 seats";
-    document.getElementById("out").innerHTML = "Our options for our five seaters are:\nKona at $55/day\nFalcon at $60/day\nPrado at $70/day"
+    document.getElementById("seats").innerHTML = "Our options for our five seaters are:\nKona at $55/day\nFalcon at $60/day\nPrado at $70/day"
   } else if (document.getElementById("7seats").checked){
     seats = "7 seats";
-    document.getElementById("out").innerHTML = "Our options for our seven seaters are:\nOutlander at $85/day"
+    document.getElementById("seats").innerHTML = "Our options for our seven seaters are:\nOutlander at $85/day"
   } else {
     seats = "8 seats";
-    document.getElementById("out").innerHTML = "Our options for our eight seaters are:\nEstima at $90/day"
+    document.getElementById("seats").innerHTML = "Our options for our eight seaters are:\nEstima at $90/day"
   }
     
 }
 
     function getInsurance(){
   if (document.getElementById("yes").checked){
-    document.getElementById("testing").innerHTML = "You will have insurance"
+    document.getElementById("insurance").innerHTML = "You will have insurance"
     insurance = 15;
   } 
 }
