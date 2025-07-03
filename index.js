abffddaaea0ea1502ea2b5f6843bf274c47aa529
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-07-03 23:20:55 +0000

    Jul 3, 2025, 11:20 PM

--- index.js
+++ index.js
@@ -4,26 +4,40 @@ let carArray = ["Bac Mono, MXC, Mini, i30, Kona, Falcon, Prado, Outlander, Estim
 let carSeats = ["1 seat, 2 seat, 4 seats, 4 seats, 5 seats, 5 seats, 5 seats, 7 seats, 8 seats"];
 let carPrices = [25, 45, 30, 35, 55, 60, 70, 85, 90];
 let insurance;
+let carConfirm = {
 
+  
+}
 
     function getSeats() {
   if (document.getElementById("1seat").checked) {
     seats = "Bac Mono";
     document.getElementById("seats").innerHTML = "Our options for our one seaters are:\nBac Mono at $25/day" 
-  } else if (document.getElementById("2seats").checked){
+  } 
+
+  else if (document.getElementById("2seats").checked){
     seats = 1;
     document.getElementById("seats").innerHTML = "Our options for our two seaters are:\nMXC at $45/day"
     document.body.append(MX5)
-  } else if (document.getElementById("4seats").checked){
+  } 
+
+  else if (document.getElementById("4seats").checked){
     seats = 2;
     document.getElementById("seats").innerHTML = "Our options for our four seaters are:\nMini at $30/day\ni30 at $35/day"
-  } else if (document.getElementById("5seats").checked){
+    document.getElementById("seats").innerHTML = 
+  } 
+
+  else if (document.getElementById("5seats").checked){
     seats = 3;
     document.getElementById("seats").innerHTML = "Our options for our five seaters are:\nKona at $55/day\nFalcon at $60/day\nPrado at $70/day"
-  } else if (document.getElementById("7seats").checked){
+  } 
+
+  else if (document.getElementById("7seats").checked){
     seats = 4;
     document.getElementById("seats").innerHTML = "Our options for our seven seaters are:\nOutlander at $85/day"
-  } else {
+  } 
+
+  else {
     seats = 5;
     document.getElementById("seats").innerHTML = "Our options for our eight seaters are:\nEstima at $90/day"
   }
