d403dca592c96bac6780dd64d64989e19ec7f390
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-07-03 00:03:53 +0000

    Jul 3, 2025, 12:03 AM

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ let seats;
 let carArray = ["Bac Mono, MXC, Mini, i30, Kona, Falcon, Prado, Outlander, Estima", car];
 let carSeats = ["1 seat, 2 seat, 4 seats, 4 seats, 5 seats, 5 seats, 5 seats, 7 seats, 8 seats", car];
 let carPrices = ["$25/day, $45/day, $30/day, $35/day, $55/day, $60/day, $70/day, $85/day, $90/day", car];
-let insurance = 0;
+let insurance;
 
 
     function getSeats() {
   if (document.getElementById("1seat").checked) {
-    seats = 1;
-    document.getElementById("seats").innerHTML = "Our options for our one seaters are:\n" +carArray[seats]+ carPrices[seats] 
+    seats = "1 seat";
+    document.getElementById("seats").innerHTML = "Our options for our one seaters are:\nBac Mono at $25/day" 
   } else if (document.getElementById("2seats").checked){
     seats = "2 seats";
-    document.getElementById("seats").innerHTML = "Our options for our two seaters are:\n" +carArray[seats]+ carPrices[seats] 
+    document.getElementById("seats").innerHTML = "Our options for our two seaters are:\nMXC at $45/day"
     document.body.append(MX5)
   } else if (document.getElementById("4seats").checked){
     seats = "4 seats";
@@ -41,7 +41,7 @@ let insurance = 0;
 }
 
 function getDays() {
-  var x = document.getElementById("mySelect").value;
+  var x = document.getElementById("days").innerHTML;
   document.getElementById("days").innerHTML = x;
 }
 
