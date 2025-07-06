b7528e3bb4a078b05e99541bf659e4eae452f0dd
aidanBLAHBLAH <23201as@hvhs.school.nz>
2025-07-06 00:31:23 +0000

    Jul 6, 2025, 12:31 AM

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let select_car = {
-  
+  type: "text"
 }
 let seats;
 let carArray = ["Bac Mono, MXC, Mini, i30, Kona, Falcon, Prado, Outlander, Estima", car];
@@ -10,7 +10,7 @@ let insurance;
 
     function getSeats() {
   if (document.getElementById("1seat").checked) {
-    document.getElementById("seats").innerHTML = "Our options for our one seaters are: Bac Mono at $25/day" 
+    document.getElementById("seats").innerHTML = "Our options for our one seaters are: Bac Mono at $25/day" +select_car
   } 
   
   else if (document.getElementById("2seats").checked){
